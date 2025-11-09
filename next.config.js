8486362ccf8c640ca7758a21421373f46acadeb0
t000085-stack <t000085@coded.edu.kw>
2025-11-09 10:44:10 +0300

    init2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
